d5267f3044c55481e831e0d8c6f06f3614ae2982
ItoShimeji <hamazakidaiti@gmail.com>
2024-11-09 12:21:46 +0900

    change top links system

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const noticeLinks = document.querySelectorAll(".links li");
+
+  noticeLinks.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      // 要素内のaタグを取得
+      const link = item.querySelector("a");
+
+      // aタグが存在し、href属性がある場合のみ実行
+      if (link && link.href) {
+        // ページをリンク先に遷移
+        window.location.href = link.href;
+      }
+    });
+  });
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,25 @@
+// ページロード時のスムーズスクロール
 window.addEventListener("load", function () {
+  scrollToHash();
+});
+
+// アンカーリンククリック時のスムーズスクロール
+document.addEventListener("click", function (event) {
+  if (event.target.tagName === "A" && event.target.hash) {
+    event.preventDefault(); // デフォルトのアンカーリンクの動作を無効化
+    history.pushState(null, null, event.target.hash); // URLにハッシュを追加
+    scrollToHash(100);
+  }
+});
+
+function scrollToHash(offset = 80) {
   const hash = window.location.hash;
   if (hash) {
     const target = document.querySelector(hash);
     if (target) {
-      const offset = 80; // ヘッダーの高さ分のオフセット
       const targetPosition =
         target.getBoundingClientRect().top + window.pageYOffset - offset;
       window.scrollTo({ top: targetPosition, behavior: "smooth" });
     }
   }
-});
+}
