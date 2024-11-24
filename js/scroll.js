b494e8c5272bb48a88a21c577e63bbeb21a6b486
ItoShimeji <hamazakidaiti@gmail.com>
2024-11-24 21:48:18 +0900

    fix loading process

--- js/scroll.js
+++ js/scroll.js
@@ -5,21 +5,34 @@ window.addEventListener("load", function () {
 
 // アンカーリンククリック時のスムーズスクロール
 document.addEventListener("click", function (event) {
-  if (event.target.tagName === "A" && event.target.hash) {
-    const linkHost = event.target.host;
-    const linkPath = event.target.pathname;
+  let target = event.target;
 
-    // 同じページ内リンクであることを確認
-    if (
-      linkHost === window.location.host &&
-      linkPath === window.location.pathname
-    ) {
-      event.preventDefault(); // デフォルトのアンカーリンクの動作を無効化
-      history.pushState(null, null, event.target.hash); // URLにハッシュを追加
-      scrollToHash(100);
-    }
+  // `LI`をクリックした場合、その子要素の`<a>`タグを取得
+  if (target.tagName === "LI" && target.children[0]?.tagName === "A") {
+    target = target.children[0]; // `<a>`タグをクリックターゲットに変更
+    target.click(); // `<a>`タグのクリックイベントを発生
+    return; // 以降の処理は不要
+  }
+
+  // `<a>`タグをクリックした場合
+  if (target.tagName === "A" && target.hash) {
+    const linkHost = target.host;
+    const linkPath = target.pathname;
+    checkSamePage(event, target, linkHost, linkPath);
   }
 });
+
+function checkSamePage(event, anchor, linkHost, linkPath) {
+  // 同じページ内リンクであることを確認
+  if (
+    linkHost === window.location.host &&
+    linkPath === window.location.pathname
+  ) {
+    event.preventDefault(); // デフォルトのアンカーリンクの動作を無効化
+    history.pushState(null, null, anchor.hash); // URLにハッシュを追加
+    scrollToHash(100);
+  }
+}
 
 function scrollToHash(offset = 80) {
   const hash = window.location.hash;
