402b23025cd227f7461317a98bf168ecaf193d36
ItoShimeji <hamazakidaiti@gmail.com>
2024-12-06 18:06:20 +0900

    fix responsive header style

--- js/headerLoader.js
+++ js/headerLoader.js
@@ -11,6 +11,7 @@ function loadHeader() {
     .then((html) => {
       header.innerHTML = html; // headerタグに読み込んだHTMLを挿入
       attachHeaderEvents(); // イベントリスナーを登録
+      swapText();
     })
     .catch((error) => {
       console.error(error);
@@ -30,5 +31,23 @@ function attachHeaderEvents() {
   }
 }
 
+let screenWidth = window.innerWidth;
+
+function swapText() {
+  const obog = document.querySelector("#ob-og");
+  screenWidth = window.innerWidth;
+
+  if (screenWidth >= 960) {
+    obog.innerHTML = "OB/OG";
+  } else {
+    obog.innerHTML = "大人になったぶんのすけ達";
+  }
+}
+
 // ページが読み込まれたらヘッダーを読み込む
 document.addEventListener("DOMContentLoaded", loadHeader);
+window.addEventListener("resize", () => {
+  if (screenWidth === window.innerWidth) return;
+  console.log("resize");
+  swapText();
+});
